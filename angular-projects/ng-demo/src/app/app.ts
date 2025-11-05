69de11a3b7f761344a7e5f1ca9df778c30a99fdb
kamal <skkamal2java@gmail.com>
2025-11-05 08:05:43 +0530

    new changes

--- angular-projects/ng-demo/src/app/app.ts
+++ angular-projects/ng-demo/src/app/app.ts
@@ -1,14 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ng-demo');
-  empName: string = 'Kamal';  
-  empName:string ="Azad";
+
+  isTrue: boolean = true;
+
+ message: string = '';
+    isClicked = false;
+    getMessage() {
+        this.isClicked = !this.isClicked;
+        if (this.isClicked) {
+            this.message = 'Welcome to GFG!!!';
+        } else {
+            this.message = '';
+        }
+    }
+
+
 }
